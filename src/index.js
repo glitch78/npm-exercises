d9049b0ea725ff233babc1886187ed39a9fae1fc
Lancelot Ellis <lancelotellis0126@gmail.com>
2019-09-16 17:01:34 -0500

    add index js

---
+++ src/index.js
@@ -0,0 +1,13 @@
+// console.log("hello world");
+//
+// import hello from './say-hello'
+//
+//
+//
+// console.log(hello.hello());
+
+
+const practice = require("./say-hello.js");
+console.log(practice.firstThing);
+console.log(practice.secondThing);
+console.log(practice.thirdThing);
---
+++ src/say-hello.js
@@ -0,0 +1,12 @@
+ // const hello =() => "this is a nice message";
+ //
+ //
+ // export default {hello};
+
+ const practice = {
+     firstThing: "does this work",
+     secondThing: "does this work",
+     thirdThing: "third thing"
+ };
+
+ module.exports = practice;
